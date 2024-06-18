91b5cd5b25f1b129e1a981f3df7947d0ba5fc7ab
Rene J Sequeira <167462385+rjs612@users.noreply.github.com>
2024-06-18 02:47:24 +0000

    update

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 let excuses = document.getElementById("excuse");
-window.addEventListener("load",generator);
-function generator(){
-    
+let who = ["The dog", "My grandma", "The Mailman", "My bird"];
+window.addEventListener("load", generator);
+function generator() {
+  for(let i in who){
+  excuses.append(i);
+}
 }
